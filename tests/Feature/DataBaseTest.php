6e964e3961d43e1a3eb72077e6b5ce4ee9d5d203
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-01-08 11:43:31 -0300

    testing database when updating a product

--- tests/Feature/DataBaseTest.php
+++ tests/Feature/DataBaseTest.php
@@ -4,6 +4,8 @@ use App\Models\Product;
 use function Pest\Laravel\assertDatabaseCount;
 use function Pest\Laravel\assertDatabaseHas;
 use function Pest\Laravel\postJson;
+use function Pest\Laravel\putJson;
+use function PHPUnit\Framework\assertSame;
 use function PHPUnit\Framework\assertTrue;
 
 it('should be able o create a product', function () {
@@ -26,7 +28,26 @@ it('should be able o create a product', function () {
 
 it('should be able to update a product', function () {
 
-})->todo();
+    $product = Product::factory()->create(['title' => 'Titulo qualquer']);
+
+    putJson(
+        route('product.update', $product),
+        ['title' => 'Atualizando o titulo']
+    )->assertOk();
+
+    assertDatabaseHas('products',[
+        'id' => $product->id,
+        'title' => 'Atualizando o titulo'
+    ]);
+
+    expect($product)
+        ->refresh()
+        ->title->toBe('Atualizando o titulo');
+
+    assertSame('Atualizando o titulo', $product->title);
+
+    assertDatabaseCount('products', 1);
+});
 
 it('should be able to delete a product', function () {
 
