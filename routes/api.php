27602cbc033d4ba87d444d8b98764ad7144f92e8
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-01-08 10:50:48 -0300

    testing page-response-json

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/products', function () {
+    $products = Product::all();
+    $products->map(fn($p) => ['title' => $p->title]);
+
+   return array_merge([
+     ['title' => 'Produto A'],
+     ['title' => 'Produto B'],
+   ], $products->toArray());
+});
