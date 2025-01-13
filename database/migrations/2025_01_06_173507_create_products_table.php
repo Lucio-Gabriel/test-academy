16eb2f686603baeb5cd209d32d09922a3c71b504
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-01-13 10:08:46 -0300

    model relationship test

--- database/migrations/2025_01_06_173507_create_products_table.php
+++ database/migrations/2025_01_06_173507_create_products_table.php
@@ -6,22 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->foreignIdFor(\App\Models\User::class, 'owner_id');
             $table->timestamps();
             $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('products');
