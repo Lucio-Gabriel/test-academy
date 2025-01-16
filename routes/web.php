7ce8006b5ddf10df015f962f69f1837b8cf1ae99
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-01-16 14:02:19 -0300

    it should test when a job is dispatche to the queue

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Jobs\ImportProductsJob;
 use App\Mail\WelcomeEmail;
 use App\Models\Product;
 use App\Models\User;
@@ -41,6 +42,12 @@ Route::delete('/products/{product}', function(Product $product) {
 Route::delete('/products/{product}/soft-delete', function(Product $product) {
     $product->delete();
 })->name('product.soft-delete');
+
+Route::post('/import-products', function () {
+    $data = request()->get('data');
+
+    ImportProductsJob::dispatch($data);
+})->name('product.import');
 
 Route::post('/sending-email/{user}', function (User $user) {
     Mail::to($user)->send(new WelcomeEmail($user));
