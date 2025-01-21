e0cfac9a72423c39e1f6c1f02d8643b0dd9624c1
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-01-21 18:39:42 -0300

    fixing old tests

--- tests/Feature/DataBaseTest.php
+++ tests/Feature/DataBaseTest.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Models\Product;
+use App\Models\User;
+use function Pest\Laravel\actingAs;
 use function Pest\Laravel\assertDatabaseCount;
 use function Pest\Laravel\assertDatabaseHas;
 use function Pest\Laravel\assertDatabaseMissing;
@@ -12,6 +14,9 @@ use function PHPUnit\Framework\assertSame;
 use function PHPUnit\Framework\assertTrue;
 
 it('should be able o create a product', function () {
+    $user = User::factory()->create();
+
+    actingAs($user);
 
     postJson(
         route('product.store'),
