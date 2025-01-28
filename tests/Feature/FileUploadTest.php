f65644b80562ab9aee19b43425494bf451e687d9
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-01-28 09:09:55 -0300

    file-upload :: it should be to import aa csv file

--- tests/Feature/FileUploadTest.php
+++ tests/Feature/FileUploadTest.php
@@ -4,6 +4,8 @@ use App\Models\User;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 use function Pest\Laravel\actingAs;
+use function Pest\Laravel\assertDatabaseCount;
+use function Pest\Laravel\assertDatabaseHas;
 use function Pest\Laravel\post;
 
 it('should be able to upload an image', function () {
@@ -22,4 +24,24 @@ it('should be able to upload an image', function () {
     Storage::disk('avatar')
         ->assertExists($file->hashName());
 
+});
+
+it('should be to import aa csv file', function () {
+    $user = User::factory()->create();
+    User::factory()->create();
+
+    actingAs($user);
+
+    $data = <<<txt
+    Product 1, 2
+    Product 2,1
+    txt;
+
+    $file = UploadedFile::fake()->createWithContent('products.csv', $data);
+
+    post(route('import-products'),['file' => $file])->assertOk();
+
+    assertDatabaseHas('products', ['title' => 'Product 1', 'owner_id' => 2]);
+    assertDatabaseHas('products', ['title' => 'Product 2', 'owner_id' => 1]);
+    assertDatabaseCount('products', 2);
 });
